460090f8a182b1ccbe2bfd4f5404cd2d9c0767b4
l2y3n2 <l2y3n2@gmail.com>
2012-09-12 08:52:46 +0800

    Problem 236 (unsolved)

--- Solution/2/30.cs
+++ Solution/2/30.cs
@@ -475,4 +475,73 @@ namespace ProjectEuler.Solution
             return Math.Round(r, 12).ToString();
         }
     }
+
+    /// <summary>
+    /// Suppliers 'A' and 'B' provided the following numbers of products for the luxury
+    /// hamper market:
+    ///
+    /// Product              'A'      'B'
+    /// Beluga Caviar        5248     640
+    /// Christmas Cake       1312    1888
+    /// Gammon Joint         2624    3776
+    /// Vintage Port         5760    3776
+    /// Champagne Truffles   3936    5664
+    ///
+    /// Although the suppliers try very hard to ship their goods in perfect condition,
+    /// there is inevitably some spoilage - i.e. products gone bad.
+    ///
+    /// The suppliers compare their performance using two types of statistic:
+    ///
+    /// The five per-product spoilage rates for each supplier are equal to the number
+    /// of products gone bad divided by the number of products supplied, for each of
+    /// the five products in turn.
+    /// The overall spoilage rate for each supplier is equal to the total number of
+    /// products gone bad divided by the total number of products provided by that
+    /// supplier.
+    ///
+    /// To their surprise, the suppliers found that each of the five per-product
+    /// spoilage rates was worse (higher) for 'B' than for 'A' by the same factor
+    /// (ratio of spoilage rates), m>1; and yet, paradoxically, the overall spoilage
+    /// rate was worse for 'A' than for 'B', also by a factor of m.
+    ///
+    /// There are thirty-five m>1 for which this surprising result could have occurred,
+    /// the smallest of which is 1476/1475.
+    ///
+    /// What's the largest possible value of m?
+    /// Give your answer as a fraction reduced to its lowest terms, in the form u/v.
+    /// </summary>
+    internal class Problem236 : Problem
+    {
+        private static int[] A = new int[] { 5248, 1312, 2624, 5760, 3936 };
+        private static int[] B = new int[] { 640, 1888, 3776, 3776, 5664 };
+
+        public Problem236() : base(236) { }
+
+        private bool Check(int badA, int badB, SmallFraction m, int id)
+        {
+            return true;
+        }
+
+        protected override string Action()
+        {
+            var list = new HashSet<SmallFraction>();
+            int sumA = A.Sum(), sumB = B.Sum();
+            SmallFraction m;
+
+            for (int badA = 5; badA <= sumA; badA++)
+            {
+                for (int badB = 5; badB <= badA * sumB / sumA; badB++)
+                {
+                    m = new SmallFraction(badA * sumB, badB * sumA);
+
+                    if (list.Contains(m))
+                        continue;
+                    if (Check(badA, badB, m, 0))
+                        list.Add(m);
+                }
+            }
+
+            return list.Max().ToString();
+        }
+    }
 }
