89e3cc2ee77b6b68993ce252b37a8e6b933d5ba2
l2y3n2 <l2y3n2@gmail.com>
2012-10-16 10:39:50 +0800

    Temporary solution

--- Solution/2/40.cs
+++ Solution/2/40.cs
@@ -89,4 +89,41 @@ namespace ProjectEuler.Solution
             return Calculate(counter, 0, nSides, targetScore).ToString();
         }
     }
+
+    /// <summary>
+    /// For a positive integer n, let σ(n) be the sum of all divisors of n, so e.g.
+    /// σ(6) = 1 + 2 + 3 + 6 = 12.
+    ///
+    /// A perfect number, as you probably know, is a number with σ(n) = 2n.
+    ///
+    /// Let us define the perfection quotient of a positive integer as p(n) = σ(n) / n.
+    ///
+    /// Find the sum of all positive integers n <= 10^18 for which p(n) has the form
+    /// k + 1⁄2, where k is an integer.
+    /// </summary>
+    internal class Problem241 : Problem
+    {
+        private static long upper = Misc.Pow(10, 18);
+
+        public Problem241() : base(241) { }
+
+        protected override string Action()
+        {
+            // http://www.numericana.com/answer/numbers.htm#hemiperfect
+            long[] nums = new long[] {
+                2, 24, 91963648, 10200236032,4320, 4680, 26208, 20427264, 197064960, 21857648640, 57575890944, 88898072401645056, 301183421949935616,
+                8910720, 17428320, 8583644160, 57629644800, 206166804480, 1416963251404800, 15338300494970880, 6275163455171297280,
+                17116004505600, 75462255348480000, 6219051710415667200,
+            };
+            long sum = 0;
+
+            foreach (var num in nums)
+            {
+                if (num <= upper)
+                    sum += num;
+            }
+
+            return sum.ToString();
+        }
+    }
 }
