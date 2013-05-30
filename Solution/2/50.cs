5498f3eb3f685722a75eb14854aa8e46854f6512
l2y3n2 <l2y3n2@gmail.com>
2013-05-30 11:19:23 +0800

    Problem 253

--- Solution/2/50.cs
+++ Solution/2/50.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using ProjectEuler.Common;
 using ProjectEuler.Common.Miscellany;
@@ -189,6 +190,91 @@ namespace ProjectEuler.Solution
             var ch = new ConvexHoles();
 
             return string.Format("{0:F1}", ch.Solve(list) / 2.0);
+        }
+    }
+
+    /// <summary>
+    /// A small child has a "number caterpillar" consisting of forty jigsaw pieces,
+    /// each with one number on it, which, when connected together in a line, reveal
+    /// the numbers 1 to 40 in order.
+    ///
+    /// Every night, the child's father has to pick up the pieces of the caterpillar
+    /// that have been scattered across the play room. He picks up the pieces at random
+    /// and places them in the correct order.
+    /// As the caterpillar is built up in this way, it forms distinct segments that
+    /// gradually merge together.
+    /// The number of segments starts at zero (no pieces placed), generally increases
+    /// up to about eleven or twelve, then tends to drop again before finishing at a
+    /// single segment (all pieces placed).
+    ///
+    /// For example:
+    ///
+    /// Piece Placed	Segments So Far
+    /// 12					1
+    /// 4					2
+    /// 29					3
+    /// 6					4
+    /// 34					5
+    /// 5					4
+    /// 35					4
+    /// …					…
+    ///
+    /// Let M be the maximum number of segments encountered during a random tidy-up of
+    /// the caterpillar.
+    /// For a caterpillar of ten pieces, the number of possibilities for each M is
+    ///
+    /// M	Possibilities
+    /// 1	512
+    /// 2	250912
+    /// 3	1815264
+    /// 4	1418112
+    /// 5	144000
+    /// so the most likely value of M is 3 and the average value is
+    /// 385643/113400 = 3.400732, rounded to six decimal places.
+    ///
+    /// The most likely value of M for a forty-piece caterpillar is 11; but what is the
+    /// average value of M?
+    ///
+    /// Give your answer rounded to six decimal places.
+    /// </summary>
+    internal class Problem253 : Problem
+    {
+        private const int length = 40;
+
+        public Problem253() : base(253) { }
+
+        private BigInteger Get(Dictionary<string, BigInteger> dict, int a, int b, int c)
+        {
+            BigInteger n;
+
+            if (a <= 0 || b <= 0 || b > c)
+                return 0;
+            if (a == 1 && b == 1)
+                return 1;
+
+            string key = string.Join(",", a, b, c);
+
+            if (dict.ContainsKey(key))
+                return dict[key];
+
+            n = b * (Get(dict, a - 1, b - 1, c) + Get(dict, a - 1, b + 1, c) + 2 * Get(dict, a - 1, b, c));
+            dict[key] = n;
+
+            return n;
+        }
+
+        protected override string Action()
+        {
+            var dict = new Dictionary<string, BigInteger>();
+            BigInteger num = 0, dem = 1;
+
+            for (int i = 1; i <= length; i++)
+            {
+                num += i * (Get(dict, length, 1, i) - Get(dict, length, 1, i - 1));
+                dem *= i;
+            }
+
+            return string.Format("{0:F6}", (double)num / (double)dem);
         }
     }
 }
